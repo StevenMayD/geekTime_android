c682796383419175a66b7e3d44e3e6962525f815
dong_shuai_wen <dongshuaiwenchina@163.com>
2023-02-16 13:47:11 +0800

    引入轮播图组件：首页fragment添加轮播视图，并使用Glide加载网络图片，进行轮播

---
+++ app/src/main/java/com/example/geektime/tools/glide/GeekAppGlideModule.java
@@ -0,0 +1,9 @@
+package com.example.geektime.tools.glide;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class GeekAppGlideModule extends AppGlideModule  {
+
+}
