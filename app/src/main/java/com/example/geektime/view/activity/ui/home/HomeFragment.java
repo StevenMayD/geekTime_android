47444fe0218f302b52f640f9cde8f6533e69ee59
dong_shuai_wen <dongshuaiwenchina@163.com>
2023-02-16 13:47:41 +0800

    引入轮播图组件：首页fragment添加轮播视图，并使用Glide加载网络图片，进行轮播

--- app/src/main/java/com/example/geektime/view/activity/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/geektime/view/activity/ui/home/HomeFragment.java
@@ -13,6 +13,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.geektime.R;
 import com.example.geektime.databinding.FragmentHomeBinding;
+import com.example.geektime.tools.glide.GlideApp;
 import com.youth.banner.Banner;
 import com.youth.banner.adapter.BannerImageAdapter;
 import com.youth.banner.holder.BannerImageHolder;
@@ -27,7 +28,8 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     private View root;
     private Banner banner; // 轮播图界面
-    private List<Integer> bannerData; // 轮播图数据：图片链接 请求后的Bitmap 数组
+//    private List<Integer> bannerData; // 轮播图数据1：加载本地图片资源 - R.drawable.数组
+    private List<String> bannerData; // 轮播图数据2：加载网络图片资源 - 链接数组
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -58,16 +60,34 @@ public class HomeFragment extends Fragment {
 
     private void loadBannerView() throws IOException {
         bannerData = new ArrayList<>();
-        bannerData.add(R.drawable.banner1);
-        bannerData.add(R.drawable.banner2);
-        bannerData.add(R.drawable.banner3);
+
+//        轮播图数据1：加载本地图片资源
+//        bannerData.add(R.drawable.banner1);
+//        bannerData.add(R.drawable.banner2);
+//        bannerData.add(R.drawable.banner3);
+
+//        轮播图数据2：加载网络图片资源
+        String image1 = "https://static001.geekbang.org/resource/image/f7/b4/f79b1fcb2e9eeb1002fd7db4b4fd10b4.jpg";
+        String image2 = "https://static001.geekbang.org/resource/image/30/86/307332b7ba9556ba1e38c358ad6aab86.jpg";
+        String image3 = "https://static001.geekbang.org/resource/image/4e/c5/4ea96e35e97f37914c1703d1cf4b69c5.jpg";
+        bannerData.add(image1);
+        bannerData.add(image2);
+        bannerData.add(image3);
 
         banner = root.findViewById(R.id.main_banner); // fragment中 使用root.findViewById获取控件
         // 适配器传入数据：必须明确参数类型，及参数数组的元素类型 才不会报错
-        banner.setAdapter(new BannerImageAdapter<Integer>(bannerData) {
+//        banner.setAdapter(new BannerImageAdapter<Integer>(bannerData) {
+        banner.setAdapter(new BannerImageAdapter<String>(bannerData) {
             @Override
-            public void onBindView(BannerImageHolder holder, Integer data, int position, int size) {
-                holder.imageView.setImageResource(data);
+//            public void onBindView(BannerImageHolder holder, Integer data, int position, int size) {
+            public void onBindView(BannerImageHolder holder, String data, int position, int size) {
+//                轮播图数据1：加载本地图片资源
+//                holder.imageView.setImageResource(data);
+
+//                轮播图数据2：加载网络图片资源
+                GlideApp.with(root)
+                        .load(data)
+                        .into(holder.imageView);
             }
         });
         banner.isAutoLoop(true); // 自动循环
